39ff9cc7095de64837195aad3cf302d7663dc892
Andrea-tailas <tituswaititu96@gmail.com>
2024-06-27 14:56:31 +0300

    fixed cors error

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import "dotenv/config";
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { booksRouter } from "./books/books.router";
-
+import {cors} from 'hono/cors'
 
 const app = new Hono()
 
+app.use('*',cors({
+  origin:'http://localhost:8000' ,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 
+}))
 
 app.get('/ok',async (c) => {
     return c.json({message: 'Hello World'})
