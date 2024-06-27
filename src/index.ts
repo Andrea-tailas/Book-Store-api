b11c02924403df58743c0ef289afd24c2d62b590
Andrea-tailas <tituswaititu96@gmail.com>
2024-06-27 14:59:36 +0300

    fixed cors error

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ import {cors} from 'hono/cors'
 const app = new Hono()
 
 app.use('*',cors({
-  origin:'http://localhost:5173' ,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin:'http://localhost:5173',
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization'],
 
 }))
 
